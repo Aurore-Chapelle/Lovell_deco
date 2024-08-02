5c4c2fa5ae25aec4b9aec1f478b6ddb43c1d7750
Aurore-Chapelle <chapelle.auroreesai@.com>
2024-08-02 17:26:52 +0200

    first commit

---
+++ Homepage.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="C:/Users/Aurore/Desktop/cdui/integration/Lovell deco/svg/prev-arrow.svg" alt="Previous"></button>',
+        nextArrow: true,
+    });
+});
