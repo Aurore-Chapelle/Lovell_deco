b492d69f3011de2f34fd906e892af7f8aacc5f43
Aurore-Chapelle <chapelle.auroreesai@.com>
2024-08-04 18:31:34 +0200

    slider + fixs

--- Homepage.js
+++ Homepage.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
-    $('.slider').slick({
+    $('.multiple-items').slick({
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
-        dots: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="C:/Users/Aurore/Desktop/cdui/integration/Lovell deco/svg/prev-arrow.svg" alt="Previous"></button>',
-        nextArrow: true,
+        autoplay: true,
+        prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+        nextArrow: '<button type="button" class="slick-next">Next</button>'
     });
 });
+
