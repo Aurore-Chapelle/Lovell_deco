aa20555373e17ba4dd1d2cfae58219bd30d71a62
Aurore-Chapelle <chapelle.auroreesai@.com>
2024-08-09 19:45:34 +0200

    creation fichiers css/section

--- js/homepage.js
+++ js/homepage.js
@@ -1,21 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function () {
   // Look for .hamburger
   const hamburger = document.querySelector(".hamburger");
   const hamburgerMenu = document.querySelector("div#navigation");
   // On click
-  hamburger.addEventListener("click", function() {
+  hamburger.addEventListener("click", function () {
     // Toggle class "is-active"
-  hamburger.classList.toggle("is-active");
+    hamburger.classList.toggle("is-active");
     // Do something else, like open/close menu
     hamburgerMenu.classList.toggle("hamburger-hidden");
   });
-    $('.multiple-items').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        autoplay: true,
-        prevArrow: '<button type="button" class="slick-prev">Previous</button>',
-        nextArrow: '<button type="button" class="slick-next">Next</button>'
-    });
+  $(".multiple-items").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+    nextArrow: '<button type="button" class="slick-next">Next</button>',
+  });
 });
-
