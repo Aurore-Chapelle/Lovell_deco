dbeaa2669f1a6f5fa3c5ffb2fee9972c3a0cfb4a
Aurore-Chapelle <chapelle.auroreesai@.com>
2024-08-09 19:24:54 +0200

    fonctionnalité hamburger ok

--- js/homepage.js
+++ js/homepage.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+  // Look for .hamburger
+  const hamburger = document.querySelector(".hamburger");
+  const hamburgerMenu = document.querySelector("div#navigation");
+  // On click
+  hamburger.addEventListener("click", function() {
+    // Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+    // Do something else, like open/close menu
+    hamburgerMenu.classList.toggle("hamburger-hidden");
+  });
     $('.multiple-items').slick({
         infinite: true,
         slidesToShow: 3,
