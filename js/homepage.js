5d0efd92f24c809d64da2063adb843fd2949d013
Aurore-Chapelle <chapelle.auroreesai@.com>
2024-08-26 11:08:07 +0200

    fix header + herosection dekstop

--- js/homepage.js
+++ js/homepage.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
   $(".multiple-items").slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
     autoplay: true,
     prevArrow: '<button type="button" class="slick-prev">Previous</button>',
     nextArrow: '<button type="button" class="slick-next">Next</button>',
